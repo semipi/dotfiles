b53ced0282b95bf5856e186bbbd2050ba534d9d1
semipi <semipi@users.noreply.github.com>
2018-06-03 21:37:23 +0200

    Add files via upload

---
+++ bootstrap.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# when problems with booting from usb stick, hit <tab> when the type of installation can be choosen and set "vga=normal".
+#
+# run dist-upgrade, update and upgrade before.
+
+sudo apt-get install git
+
+sudo apt-get install texlive-full
+sudo apt-get install latex-beamer
+
+sudo apt-get install i3
+
+mkdir -p ~/dev/bootstrap
+cd ~/dev/bootstrap
+git clone https://github.com/semipi/conf.git
+
+cp .bash_aliases .vimrc ~
+cp .i3/config ~/.i3/config
+echo 'export EDITOR=vim' >> ~/.bashrc
+
+sudo apt-get install python-pip
+sudo apt-get install python3-pip
+
+mkdir -p ~/tmp/ranger
+cd ~/tmp/ranger
+git clone https://github.com/ranger/ranger.git
+cd ranger
+sudo make install
+
+sudo apt-get intall python-dev
+sudo pip install glances
+pip install --upgrade glances
+
+sudo apt-get install vlc
