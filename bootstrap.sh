5aee230ae2c0f055c59f08be5ce0fd9a04175f30
semipi <semipi@users.noreply.github.com>
2019-01-13 13:55:23 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,28 +4,43 @@
 #
 # run dist-upgrade, update and upgrade before.
 
-sudo apt-get install git
+sudo apt-get --assume-yes install git
 
-sudo apt-get install texlive-full
-sudo apt-get install latex-beamer
+# sudo apt-get --assume-yes install texlive-full
+# sudo apt-get --assume-yes install latex-beamer
 
-sudo apt-get install cmake
+sudo apt-get --assume-yes install cmake
 
-sudo apt-get install tree
+sudo apt-get --assume-yes install tree
 
-sudo apt-get install i3
+sudo apt-get --assume-yes install i3
 
-mkdir -p ~/dev/bootstrap
-cd ~/dev/bootstrap
-git clone https://github.com/semipi/conf.git
+# mkdir -p ~/dev/bootstrap
+# cd ~/dev/bootstrap
+# git clone https://github.com/semipi/conf.git
 
+cd ~
+mv Downloads downloads
+mv Pictures pictures
+mv Documents documents
+mv Videos videos
+mv Templates templates
+mv Music music
+mv Pictures pictures
+
+cp ~/dev/dotfiles/.config/user-dirs.dirs ~/.config/user-dirs.dirs
+
+cd ~/dev/dotfiles
 sudo apt-get install vim
-cp .bash_aliases .vimrc ~
+cp .bashrc .bash_aliases .vimrc ~
+mkdir ~/.i3
 cp .i3/config ~/.i3/config
+cp .i3/i3status.conf ~/.i3/i3status.conf
+
 echo 'export EDITOR=vim' >> ~/.bashrc
 
-sudo apt-get install python-pip
-sudo apt-get install python3-pip
+sudo apt-get --assume-yes install python-pip
+sudo apt-get --assume-yes install python3-pip
 
 mkdir -p ~/tmp/ranger
 cd ~/tmp/ranger
@@ -33,10 +48,10 @@ git clone https://github.com/ranger/ranger.git
 cd ranger
 sudo make install
 
-sudo apt-get install python-dev
+sudo apt-get --assume-yes install python-dev
 sudo pip install glances
 pip install --upgrade glances
 
-sudo apt-get install vlc
-sudo apt-get install smbclient
+sudo apt-get --assume-yes install vlc
+sudo apt-get --assume-yes install smbclient
 
