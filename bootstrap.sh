ca60086896498dca626442a48caa60713146bc4c
semipi <e-funktion@gmx.de>
2022-02-01 22:18:37 +0100

    update bootstrap to arch

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,69 +1,40 @@
 #!/bin/bash
 
 # when problems with booting from usb stick, hit <tab> when the type of installation can be choosen and set "vga=normal".
-#
-# run dist-upgrade, update and upgrade before.
 
-sudo apt-get update
-sudo apt-get upgrade
-
-sudo apt-get --assume-yes install git
-
-sudo apt-get --assume-yes install g++
-sudo apt-get --assume-yes install build-essential
-sudo apt-get --assume-yes install cmake
-sudo apt-get --assume-yes install mesa-common-dev
-sudo apt-get --assume-yes install libglu1-mesa-dev
-sudo apt-get --assume-yes install libfontconfig1
-
-sudo apt-get --assume-yes install qt5-default
-sudo apt-get --assume-yes install qttools5-dev-tools
-sudo apt-get --assume-yes install libeigen3-dev
-
-sudo apt-get --assume-yes install texlive-full
-sudo apt-get --assume-yes install latex-beamer
-
-
-sudo apt-get --assume-yes install tree
-sudo apt-get --assume-yes install i3
+sudo pacman -Sy
+sudo pacman -S --noconfirm git
+sudo pacman -S --noconfirm cmake
+sudo pacman -S --noconfirm gcc
+sudo pacman -S --noconfirm python
+sudo pacman -S --noconfirm python-numpy
+sudo pacman -S --noconfirm python-scipy
+sudo pacman -S --noconfirm python-matplotlib
+sudo pacman -S --noconfirm boost
+sudo pacman -S --noconfirm openmp
+sudo pacman -S --noconfirm python-cvxopt
+sudo pacman -S --noconfirm python-yaml
+sudo pacman -S --noconfirm fftw
+sudo pacman -S --noconfirm eigen
+sudo pacman -S --noconfirm nvim
 
 cd ~
-mv Downloads downloads
-mv Pictures pictures
+mv Desktop desktop
 mv Documents documents
-mv Videos videos
-mv Templates templates
+mv Downloads downloads
 mv Music music
 mv Pictures pictures
+mv Public public
+mv Templates templates
+mv Videos videos
 
 cp ~/dev/dotfiles/.config/user-dirs.dirs ~/.config/user-dirs.dirs
 
 cd ~/dev/dotfiles
-sudo apt-get --assume-yes install emacs
-sudo apt-get --assume-yes install vim
 cp .bashrc .bash_aliases .vimrc ~
+
 mkdir ~/.i3
 cp .i3/config ~/.i3/config
 cp .i3/i3status.conf ~/.i3/i3status.conf
 
 echo 'export EDITOR=vim' >> ~/.bashrc
-
-sudo apt-get --assume-yes install python-pip
-sudo apt-get --assume-yes install python3-pip
-
-mkdir -p ~/tmp/ranger
-cd ~/tmp/ranger
-git clone https://github.com/ranger/ranger.git
-cd ranger
-sudo make install
-
-sudo apt-get --assume-yes install python-dev
-sudo pip install glances
-pip install --upgrade glances
-
-sudo apt-get --assume-yes install vlc
-sudo apt-get --assume-yes install samba
-sudo apt-get --assume-yes install cifs-utils
-
-
-sudo apt autoremove
